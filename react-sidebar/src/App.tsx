af081f6c8020b777b6e418fd17ef84246279cc96
arpco13 <arpcothirteen@outlook.com>
2025-10-29 16:53:30 +0330

    fixed layout

--- react-sidebar/src/App.tsx
+++ react-sidebar/src/App.tsx
@@ -3,8 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import TextInput from "./components/TextInput";
 import TextArea from "./components/TextArea";
 import "./App.css";
-import { useState } from "react";
-// Add these basic page components
+
 function Home() {
   return <div className="p-6">Home Page</div>;
 }
@@ -19,38 +18,34 @@ function Contact() {
 
 function App() {
   return (
-    <>
-      <div className="m-10 flex items-center">
-        <TextArea label="name" />
-      </div>
-      <div className="m-10 flex items-center">
-        <TextArea errorMessage="wrong" />
-      </div>
+    <div className="flex h-screen">
+      {/* Sidebar always visible on the left */}
+      <Sidebar />
 
-      <div className="m-10 flex items-center ">
-        <TextInput
-          label=" نام خانوادگی"
-          placeholder="نام خانوادگی خود را وارد کنید"
-        />
-      </div>
-      <div className="m-10 flex items-center">
-        <TextInput label="نام" placeholder="نام خود را وارد کنید" />
-      </div>
-      <div className="m-10 flex items-center">
-        <TextInput label="ایمیل" errorMessage="ایمیل اشتباه است" />
-      </div>
+      {/* Main content area */}
+      <main className="flex-1 overflow-auto flex flex-col items-center justify-center bg-gray-50">
+        {/* Centered content */}
+        <div className="space-y-6 w-full max-w-md">
+          <TextArea label="name" />
+          <TextArea errorMessage="wrong" />
+          <TextInput
+            label="نام خانوادگی"
+            placeholder="نام خانوادگی خود را وارد کنید"
+          />
+          <TextInput label="نام" placeholder="نام خود را وارد کنید" />
+          <TextInput label="ایمیل" errorMessage="ایمیل اشتباه است" />
+        </div>
 
-      {/* <div className="app flex h-screen">
-        <Sidebar />
-        <main className="flex-1 overflow-auto">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-      </div> */}
-    </>
+        {/* Example: if you later enable routes */}
+        {/* 
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        */}
+      </main>
+    </div>
   );
 }
 
