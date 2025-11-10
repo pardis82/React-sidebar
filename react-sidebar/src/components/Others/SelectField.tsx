bf4a3a80bac7c5ed45031c30c37e66e883432292
arpco13 <arpcothirteen@outlook.com>
2025-11-10 15:15:51 +0330

    minor css changes

--- react-sidebar/src/components/Others/SelectField.tsx
+++ react-sidebar/src/components/Others/SelectField.tsx
@@ -200,13 +200,13 @@ export default function Select(props: SelectProps) {
                       {searchable && (
                         <div className="px-2 pb-1 bg-white sticky top-0 ">
                           <div className="relative">
-                            <CiSearch className="absolute right-1 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
+                            <CiSearch className="absolute transform rotate-360 right-1 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none" />
                             <input
                               type="text"
                               value={query}
                               onChange={(e) => setQuery(e.target.value)}
                               placeholder="جستجو..."
-                              className="w-full border border-gray-300 rounded-md px-5 py-2 text-sm focus:ring-1 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
+                              className="w-full border border-gray-300 rounded-md px-5 py-2 text-sm focus:ring-1/2 focus:border-purple-500 focus:outline-none focus:ring-purple-500"
                             />
                           </div>
                         </div>
@@ -226,7 +226,7 @@ export default function Select(props: SelectProps) {
                             onClick={handleSelectAll}
                             className={clsx(
                               "relative cursor-default select-none py-2 px-2 border-b border-gray-200 w-full text-right",
-                              "hover:text-purple-900 text-gray-900"
+                              "hover:text-purple-600 text-gray-900"
                             )}
                           >
                             <span
@@ -257,7 +257,7 @@ export default function Select(props: SelectProps) {
                             className={({ focus, selected }) =>
                               clsx(
                                 "relative cursor-default select-none py-2 px-2",
-                                focus ? "text-purple-900" : "text-gray-900",
+                                focus ? "text-purple-600" : "text-gray-900",
                                 selected && "text-purple-600"
                               )
                             }
